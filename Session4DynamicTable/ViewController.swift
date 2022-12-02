ae0a52184108eb219bd317a9cf5af5e3bf588fbb
Ali Akbar <ali.akbar@telkomsel.co.id>
2022-12-02 21:28:01 +0700

    Initial Commit

---
+++ Session4DynamicTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Session4DynamicTable
+//
+//  Created by Ali Akbar on 02/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
